ed6f4b496d75f60cc325ff74f8bd5d1e4797433b
Dmitri Zaitsev <dmitri14@gmail.com>
2016-01-23 17:59:30 +0000

    Fix single file copy

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var shells = require('shelljs')
 module.exports = function (filesList, dest) {
   console.log(chalk.blue('\n= Begin copying files'))
 
+  // if single file or directory, create an array
+  if (! filesList.forEach) {
+  	filesList = [filesList];
+  }
+
   filesList.forEach(function (file) {
     console.log(chalk.green(' - Copying file or directory:'), chalk.red(file))
 
--- test.js
+++ test.js
@@ -5,3 +5,8 @@ test('copy files and shout', t => {
 	fn(['file1', 'file2', 'dir'], 'dest');
 	t.end();
 });
+
+test('copy single files', t => {
+	fn('file', 'dest');
+	t.end();
+});
