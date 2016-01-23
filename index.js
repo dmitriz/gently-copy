293c86be6cd1154b786a47333b2200d7b62b1d3e
Dmitri Zaitsev <dmitri14@gmail.com>
2016-01-23 18:08:58 +0000

    Fix style

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ module.exports = function (filesList, dest) {
   console.log(chalk.blue('\n= Begin copying files'))
 
   // if single file or directory, create an array
-  if (! filesList.forEach) {
-  	filesList = [filesList];
+  if (!filesList.forEach) {
+    filesList = [filesList]
   }
 
   filesList.forEach(function (file) {
