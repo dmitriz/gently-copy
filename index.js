a65d61e6065821b223ec52b782f3c9c1c8776289
dmitriz <dmitri14@gmail.com>
2016-05-15 16:12:41 +0100

    Back to shelljs

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 'use strict'
 
 var chalk = require('chalk')
-var cpy = require('cpy')
-// var shells = require('shelljs')
+// var cpy = require('cpy')
+var shells = require('shelljs')
 
 module.exports = function (filesList, dest) {
   console.log(chalk.blue('\n= Begin copying files'))
@@ -17,8 +17,8 @@ module.exports = function (filesList, dest) {
     console.log(chalk.green(' - Copying file or directory:'), chalk.red(file))
 
 		// https://github.com/shelljs/shelljs#javascript
-    // shells.cp('-R', file, dest)
-    cpy(file, dest, {overwrite: false})
+    shells.cp('-R', file, dest)
+    // cpy(file, dest, {overwrite: false})
   })
 
   console.log(chalk.blue('= End copying files\n'))
