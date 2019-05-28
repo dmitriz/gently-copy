0db9f8feae3bd7d927771cef64b4f5f9b06206bb
Mike Hingley <computa_mike@hotmail.com>
2019-05-28 15:43:12 +0100

    Enforcing standard compliance.

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 // index.js
 'use strict'
 const fs = require('fs-extra')
-const path = require('path');
+const path = require('path')
 const chalk = require('chalk')
 
 gentlyCopy.read = function (file) {
-	return fs.readFileSync(file, 'utf8')
+  return fs.readFileSync(file, 'utf8')
 }
 
 module.exports = gentlyCopy
 
-function gentlyCopy(filesList, dest, opt) {
+function gentlyCopy (filesList, dest, opt) {
+  if (typeof opt !== 'object') {
+    opt = { overwrite: false }
+  }
+  console.log(chalk.blue('\n= Begin copying files'))
 
-	if (typeof opt !== 'object') {
-		opt = { overwrite: false }
-	}
-	console.log(chalk.blue('\n= Begin copying files'))
+  // if single file or directory, create an array
+  if (!filesList.forEach) {
+    filesList = [filesList]
+  }
+  let destinationAddress = dest
+  let destination = fs.existsSync(destinationAddress)
+  let destinationIsFile = destination ? fs.lstatSync(destinationAddress).isFile() : false
 
-	// if single file or directory, create an array
-	if (!filesList.forEach) {
-		filesList = [filesList]
-	}
-	let destinationAddress = dest
-	let destination = fs.existsSync(destinationAddress)
-	let destinationIsFile = destination ? fs.lstatSync(destinationAddress).isFile() : false
+  filesList.forEach(function (file) {
+    let calculatedDestinationAddress = destinationAddress
+    let sourceIsFile = fs.lstatSync(file).isFile()
 
-	filesList.forEach(function (file) {
-		let calculatedDestinationAddress = destinationAddress
-		let sourceIsFile = fs.lstatSync(file).isFile()
+    // calculate destination...
+    if (sourceIsFile && !destinationIsFile) {
+      calculatedDestinationAddress = destinationAddress + path.sep + file
+    }
 
-		// calculate destination...
-		if (sourceIsFile && !destinationIsFile) {
-			calculatedDestinationAddress = destinationAddress + path.sep + file
-		}
+    if (opt.overwrite && opt.overwrite === true) {
+      console.log(chalk.green(' - Overwriting file or directory:'), chalk.red(file))
+    } else {
+      console.log(chalk.green(' - Copying file or directory:'), chalk.red(file))
+    }
+    fs.copySync(file, calculatedDestinationAddress, opt)
+  })
 
-		if (opt.overwrite && opt.overwrite === true) {
-			console.log(chalk.green(' - Overwriting file or directory:'), chalk.red(file))
-		} else {
-			console.log(chalk.green(' - Copying file or directory:'), chalk.red(file))
-		}
-		fs.copySync(file, calculatedDestinationAddress, opt)
-	  })
-
-	console.log(chalk.blue('= End copying files\n'))
+  console.log(chalk.blue('= End copying files\n'))
 }
